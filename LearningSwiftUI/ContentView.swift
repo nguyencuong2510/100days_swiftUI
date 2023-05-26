5c33d6103c1f631a4fad541b783a69d8179eb4fc
nguyencuong2510 <cuong251095@gmail.com>
2023-05-26 16:45:30 +0700

    Initial Commit

---
+++ LearningSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//  Created by Apple developer on 26/05/2023
+//  Copyright © 2023. All Rights Reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//  Created by Apple developer on 26/05/2023
+//  Copyright © 2023. All Rights Reserved.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
